3fe036437fe28b1f34813c29f24adf5c04c80f4b
Dan Timoney <dtimoney@att.com>
2022-10-31 09:19:15 -0400

    Get oracle indexes and send response

--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,7 @@ import Web3 from 'web3';
 import express from 'express';
 
 
+
 let config = Config['localhost'];
 let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
 web3.eth.defaultAccount = web3.eth.accounts[0];
@@ -12,8 +13,18 @@ let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddre
 let oracles = Oracles.oracles;
 let num_oracles = Oracles.num_oracles;
 
+// Get indexes
+async function getIndexes() {
+
+  for (var i = 0 ; i < num_oracles ; i++) {
+    oracles[i].indexes = await flightSuretyApp.methods
+          .getMyIndexes()
+          .call({from: oracles[i].account});
+  }
+}
 
 // Register oracles
+var numRegistered = 0;
 for (var i = 0 ; i < num_oracles ; i++) {
   flightSuretyApp.methods
   .registerOracle()
@@ -24,8 +35,13 @@ for (var i = 0 ; i < num_oracles ; i++) {
      gasPrice: 100000000000 
     },
    (error, result) => {
+    // Once all registrations are sent, get indexes
+    numRegistered += 1;
+    if (numRegistered == num_oracles) {
+      getIndexes();
+    }
 
-   })
+   });
 }
 
 
@@ -37,22 +53,43 @@ flightSuretyApp.events.OracleRequest({
     console.log("Error:");
     console.log(error);
   } 
-  console.log("Event:");
-  console.log(event);
+  // console.log("Event:");
+  // console.log(event);
 
-  console.log("Airline: " +event.returnValues.airline);
 
   let index = event.returnValues.index;
   let airline = event.returnValues.airline;
   let flight = event.returnValues.flight;
   let timestamp = event.returnValues.timestamp;
 
+  console.log("Got an Oracle request");
+  console.log("index: "+index);
+  console.log("airline:"+airline);
+  console.log("flight:"+flight);
+  console.log("timestamp:"+timestamp);
+
   for (var j = 0 ; j < num_oracles ; j++) {
-    if (!oracles[j].indexes) {
-      // Get indexes
+    if (oracles[j].indexes) {
+      if (oracles[j].indexes.includes(index)) {
+        console.log("Oracle["+j+"]: this is one of mine!");
+
+        flightSuretyApp.methods
+        .submitOracleResponse()
+        .send(index, 
+             airline,
+             flight,
+             timestamp,
+             oracles[j].return_status, 
+             {from: oracles[j].account,
+              gas: 4712388,             
+              gasPrice: 100000000000 
+               })
+
+      } 
+    } else {
+      console.log("Oracle["+j+"]: I have no indexes!");
     }
 
-    // Check 
   }
 });
 
